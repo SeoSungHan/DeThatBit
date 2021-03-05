729209a34319f8d82a2ae3dbc6512d52decd7246
김서현 <2016024766@hanyang.ac.kr>
2021-03-05 13:49:34 +0900

    about & freeboard-list 수정

--- free_board/views.py
+++ free_board/views.py
@@ -4,9 +4,9 @@ from .models import Free_Post
 
 class Free_List(ListView):
     model=Free_Post
-    template_name='free_board/list_example.html'
+    template_name='free_board/list.html'
     ordering='-pk'
 
 class Free_Detail(DetailView):
     model=Free_Post
-    template_name='free_board/detail_example.html'
+    template_name='free_board/detail.html'
