fc615a53e69dc879eedf7c5147327894c174c5a2
오성준 <2016024893@hanyang.ac.kr>
2021-03-09 01:10:26 +0900

    CRUD구현(1) - 성능만 구현함

--- free_board/urls.py
+++ free_board/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns=[
     path('<int:pk>/',views.Free_Detail.as_view()),
     path('',views.Free_List.as_view()),
+    path('create/',views.Free_Post_Create,name="free_create"),
+    path('<int:pk>/update/',views.Free_Post_Update,name="free_update"),
+    path('<int:pk>/delete/',views.Free_Post_Delete,name="free_delete"),
 ]
--- free_board/views.py
+++ free_board/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Free_Post
-
-
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+ 
 class Free_List(ListView):
     model = Free_Post
     template_name = 'free_board/list.html'
@@ -12,3 +14,42 @@ class Free_List(ListView):
 class Free_Detail(DetailView):
     model = Free_Post
     template_name = 'free_board/detail.html'
+
+@login_required
+def Free_Post_Create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('../', pk=post.pk)
+    else:
+        form = PostForm()
+        return render(request, 'free_board/edit.html', {'form': form})
+
+
+@login_required
+def Free_Post_Update(request, pk):
+    post = get_object_or_404(Free_Post, pk=pk)
+    if request.user==post.author:
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.save()
+                return redirect('../', pk=post.pk)
+        else:
+            form = PostForm(instance=post)
+            return render(request, 'free_board/edit.html', {'form': form})
+    else: return redirect('../')
+
+@login_required
+def Free_Post_Delete(request, pk):
+    post = Free_Post.objects.get(pk=pk)
+    if request.user==post.author:
+        post.delete()
+        return redirect('../../')
+    else:
+        return redirect('../')
