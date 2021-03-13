d4dfd537d44a6b010c3a4fa02f48bb774785c46f
김서현 <2016024766@hanyang.ac.kr>
2021-03-13 18:49:56 +0900

    background 변경

--- free_board/static/free_board/comment_edit.js
+++ free_board/static/free_board/comment_edit.js
@@ -1,21 +1,20 @@
-const comments=document.querySelector('.comment');
+const comments = document.querySelector(".comment");
 
-function handleClick(e){
-    const e_type=e.target.name;
-    const pk=e.target.value;
-    const tmp='.comment-edit-form-'+pk;
-    const form=document.querySelector(tmp);
-    
-    if(e_type==="try-edit"){    
-        form.classList.remove('hidden');
-    }
-    else if(e_type==="edit-back"){
-        form.classList.add('hidden');
-    }
+function handleClick(e) {
+  const e_type = e.target.name;
+  const pk = e.target.value;
+  const tmp = ".comment-edit-form-" + pk;
+  const form = document.querySelector(tmp);
+
+  if (e_type === "try-edit") {
+    form.classList.remove("hidden");
+  } else if (e_type === "edit-back") {
+    form.classList.add("hidden");
+  }
 }
 
-function main(){
-    comments.addEventListener("click", handleClick);
+function main() {
+  comments.addEventListener("click", handleClick);
 }
 
 main();
