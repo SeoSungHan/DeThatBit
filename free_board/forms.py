a61557c5e823a902f9b3e1c621ad7f68cd048395
김서현 <2016024766@hanyang.ac.kr>
2021-03-14 04:01:16 +0900

    free_board list와 detail 변경

--- free_board/forms.py
+++ free_board/forms.py
@@ -2,12 +2,14 @@ from django import forms
 
 from .models import Free_Post, Free_Comment
 
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Free_Post
         fields = ('title', 'content',)
 
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Free_Comment
-        fields  =('text',)
+        fields = ('text',)
