9257aaf5d340b47564346cb2e651ea09ac8e8e24
김서현 <2016024766@hanyang.ac.kr>
2021-04-06 22:10:52 +0900

    free & review 검색창 수정

--- albums/models.py
+++ albums/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 import os
 
-class Albums(models.Model):
-    artist=models.CharField(max_length=30)
-    a_type=models.CharField(max_length=10)
-    album=models.CharField(max_length=30)
-    date=models.DateField()
-    cover=models.CharField(max_length=500)
-    link=models.CharField(max_length=500, null = True)
 
-    rating=models.FloatField(default=0)
-    reviews=models.IntegerField(default=0)
+class Albums(models.Model):
+    artist = models.CharField(max_length=30)
+    a_type = models.CharField(max_length=10)
+    album = models.CharField(max_length=30)
+    date = models.DateField()
+    cover = models.CharField(max_length=500)
+    link = models.CharField(max_length=500, null=True)
+
+    rating = models.FloatField(default=0)
+    reviews = models.IntegerField(default=0)
 
     def __str__(self):
         return self.artist+'::'+self.album
