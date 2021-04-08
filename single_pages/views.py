f0f980f8560f9f0f9091dbe59756f51687060d24
김서현 <2016024766@hanyang.ac.kr>
2021-04-08 17:08:13 +0900

    free & review detail의 delete 수정

--- single_pages/views.py
+++ single_pages/views.py
@@ -11,17 +11,19 @@ from free_board.models import Free_Post
 from django.views.decorators.http import require_http_methods
 # Create your views here.
 
+
 def landing(request):
-    recent_album=Albums.objects.all().order_by('-date')[0:6]
-    recent_review=Review_Post.objects.all().order_by('-pk')[0:10]
-    recent_free=Free_Post.objects.all().order_by('-pk')[0:10]
+    recent_album = Albums.objects.all().order_by('-date')[0:6]
+    recent_review = Review_Post.objects.all().order_by('-pk')[0:10]
+    recent_free = Free_Post.objects.all().order_by('-pk')[0:10]
     return render(
         request,
         'single_pages/landing.html',
         {'album': recent_album,
-        'review': recent_review,
-        'free': recent_free}
+         'review': recent_review,
+         'free': recent_free}
     )
+
 
 def about_this(request):
     return render(
@@ -29,18 +31,22 @@ def about_this(request):
         'single_pages/about_this.html'
     )
 
+
 def sign_up(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
-        if form.is_valid(): # 회원가입 폼에서 적어 보낸 요청이 유효한지 검사한다.
-            user = form.save() # 유효한 내용이면 이 회원 정보를 데이터베이스에 저장한다. 그 유저 정보를 리턴한다.
+        if form.is_valid():  # 회원가입 폼에서 적어 보낸 요청이 유효한지 검사한다.
+            # 유효한 내용이면 이 회원 정보를 데이터베이스에 저장한다. 그 유저 정보를 리턴한다.
+            user = form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-            user_login(request, user) # 유저 정보를 이용해 로그인한다.
-        return redirect('single_pages:landing')
-    	# redirect 시 urls.py의 <app_name>:<name>으로 요청을 보낸다.
+            user_login(request, user)  # 유저 정보를 이용해 로그인한다.
+            return redirect('single_pages:landing')
+        # redirect 시 urls.py의 <app_name>:<name>으로 요청을 보낸다.
+        else:
+            redirect('/')
     else:
-        form = UserCreationForm() # 비어있는 회원가입 폼을 생성한다.
+        form = UserCreationForm()  # 비어있는 회원가입 폼을 생성한다.
     return render(request, 'single_pages/sign_up.html', {'form': form})
-    	# forms.html 파일을 렌더한다. 이때 위에서 생성한 회원가입 폼을 'form'이라는 이름으로 함께 보낸다.(딕셔너리)
+    # forms.html 파일을 렌더한다. 이때 위에서 생성한 회원가입 폼을 'form'이라는 이름으로 함께 보낸다.(딕셔너리)
